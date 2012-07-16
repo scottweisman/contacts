36e79c3d215f784197cd0693cdace5cf26b3425e
Scott Weisman <sweisman18@gmail.com>
2012-07-15 23:15:11 -0500

    added before filter for number of contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,12 @@
 class ContactsController < ApplicationController
   before_filter :authorize
+  before_filter :check_number_of_contacts, :only => :create
+  
+  def check_number_of_contacts
+    if current_user.group.contacts.length == 20
+      redirect_to new_subscription_path
+    end
+  end
 
   def index
     @contacts = current_group.contacts.order("last_name")
@@ -18,16 +25,12 @@ class ContactsController < ApplicationController
   end
 
   def create
-    if current_user.group.contacts.length == 20
-      redirect_to edit_user_path(current_user)
+    @contact = current_group.contacts.new(params[:contact])   
+    @contact.user_id = current_user.id
+    if @contact.save
+      redirect_to contacts_path, notice: 'Contact was successfully created.'
     else
-      @contact = current_group.contacts.new(params[:contact])   
-      @contact.user_id = current_user.id
-      if @contact.save
-        redirect_to contacts_path, notice: 'Contact was successfully created.'
-      else
-        render action: "new"
-      end
+      render action: "new"
     end
   end
 
