263379a01767b4817475e7bb9706441a9a9730ea
Scott Weisman <sweisman18@gmail.com>
2012-07-26 00:24:16 -0500

    stripe customer id saving to user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,8 @@ class Subscription < ActiveRecord::Base
   
   attr_accessor :stripe_card_token
   
+  after_create :update_user_attributes
+  
   def save_with_payment
     if valid?
       customer = Stripe::Customer.create(email: email, plan: plan_id, card: stripe_card_token)
@@ -14,7 +16,17 @@ class Subscription < ActiveRecord::Base
     end
   rescue Stripe::InvalidRequestError => e
     logger.error "Stripe error while creating customer: #{e.message}"
-    errors.add :base, "There was a problem with your credit card."
+    errors.add :base, "There was a problem with your credit card. Please try again."
     false  
   end
+  
+  private
+  
+  def update_user_attributes
+    user = User.find_by_email(self.email)
+    user.update_attribute(:plan_id, self.plan_id)
+    user.update_attribute(:stripe_customer_token, self.stripe_card_token)
+    user.save
+  end
+  
 end
