311d6d62c4025d0d66c3783740077b8475792739
Scott Weisman <sweisman18@gmail.com>
2012-07-26 22:59:20 -0500

    testing faster jquery moves

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -16,7 +16,7 @@ $(function() {
 	$('#website_opt_input').hide();
 	$('div#website_opt_text').click(function () {
 		$('div#website_opt_text').hide();
-		$('#website_opt_input').show("slow");
+		$('#website_opt_input').show();
 	});
 	
 	$('#facebook_opt_input').hide();
