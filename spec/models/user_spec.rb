1d40f1a37d1627a5c0dfa6296e045f674038e671
Scott Weisman <sweisman18@gmail.com>
2013-02-09 10:45:01 -0600

    updated user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it 'creates a user with valid attributes' do
+		user = User.new(full_name: 'Lucy Example', email: 'lucym123@example.com', password: 'secret')
+		user.should be_valid
+	end
+
+	it "is invalid without a full name" do
+		user = User.new(full_name: '', email: 'lucym123@example.com', password: 'secret')
+		user.should_not be_valid
+	end
+
 end
